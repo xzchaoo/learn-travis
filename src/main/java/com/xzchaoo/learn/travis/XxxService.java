82256df7b74ad155aa399c5729743f59e07fb34d
xzchaoo <70862045@qq.com>
2017-05-02 12:06:53 +0800

    初始化

---
+++ src/main/java/com/xzchaoo/learn/travis/XxxService.java
@@ -0,0 +1,10 @@
+package com.xzchaoo.learn.travis;
+
+/**
+ * Created by Administrator on 2017/5/2.
+ */
+public class XxxService {
+	public String hello(String name) {
+		return "hello " + name;
+	}
+}
---
+++ src/test/java/com/xzchaoo/learn/travis/XxxServiceTest.java
@@ -0,0 +1,16 @@
+package com.xzchaoo.learn.travis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Administrator on 2017/5/2.
+ */
+public class XxxServiceTest {
+	@Test
+	public void testHello() {
+		XxxService s = new XxxService();
+		assertEquals("hello world", s.hello("world"));
+	}
+}
